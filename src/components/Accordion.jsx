2d75db9d8efaac9d184c679f465509753892cb33
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-24 03:03:17 +0100

    create the Accordion component and the data

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import data from "../components/data";
 import "./style.css";
 
 export const Accordion = () => {
+
+    const [selected, setSelected] = useState(null);
+
   return (
-    <div>Accordion</div>
+    <div>
+        <h2 style={{textDecoration: "underline", color: "red", fontWeight: "bold"}}>Accordion</h2>
+        <div className='wrapper'>
+            <div className='accordion'>
+                {
+                    data && data.length > 0 ? (
+                        data.map((accorItem) => (
+                            <div className='item'>
+                                <div className='title' onClick={""}>
+                                    <h4>{accorItem.question}</h4>
+                                    <span>+</span>
+                                </div>
+                            </div>
+                        ))
+                    ):(
+                    <div>Data not found</div>
+                    )}
+            </div>
+        </div>
+    </div>
   )
 }
