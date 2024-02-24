d08fc47bd8cce85d4514b980e3f68119aec9597f
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-24 02:23:31 +0100

    create the Accordion component and the data

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import data from "../components/data";
+import "./style.css";
+
+export const Accordion = () => {
+  return (
+    <div>Accordion</div>
+  )
+}
---
+++ src/components/data.js
@@ -0,0 +1,28 @@
+const data =[
+    {
+        id: "1",
+        question: "Qu'est-ce que la photosynthèse ?",
+        answer: "Processus où les plantes convertissent la lumière en énergie pour produire de la nourriture et libérer de l'oxygène.",
+    },
+    {
+        id: "2",
+        question: "Quelle est la différence entre un virus et une bactérie ?",
+        answer: "Les virus nécessitent une cellule hôte pour se reproduire, tandis que les bactéries sont des organismes unicellulaires autonomes.",
+    },
+    {
+        id: "3",
+        question: "Qu'est-ce que la théorie de la relativité restreinte ?",
+        answer: "Elle postule que les lois de la physique sont les mêmes pour tous les observateurs et inclut des phénomènes comme la dilatation du temps.",
+    },
+    {
+        id: "4",
+        question: "Comment fonctionne la cryptographie asymétrique ?",
+        answer: " Elle utilise deux clés, l'une pour chiffrer et l'autre pour déchiffrer, assurant une communication sécurisée.",
+    },
+    {
+        id: "5",
+        question: "Qu'est-ce que la déforestation et quelles en sont les conséquences ?",
+        answer: "C'est la conversion des forêts en terres non forestières, entraînant la perte de biodiversité et contribuant au changement climatique.",
+    },
+];
+export default data;
