0c829af2ddc5a275088fbb0926888debe643ba9b
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-25 16:30:50 +0100

    arrowdown

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from 'react'
 import data from "../components/data";
 import "./style.css";
+import { FaArrowUp } from "react-icons/fa";
+import { FaArrowDown } from "react-icons/fa6";
 
 export const Accordion = () => {
 
     const [selected, setSelected] = useState(null);
 
+
+    const handleSingleQuestion = (getCuurentId) => {
+        setSelected(getCuurentId === selected ? null : getCuurentId)
+    };
+    
+
+
+
   return (
+
+
     <div>
         <h2 style={{textDecoration: "underline", color: "red", fontWeight: "bold"}}>Accordion</h2>
         <div className='wrapper'>
@@ -14,11 +26,16 @@ export const Accordion = () => {
                 {
                     data && data.length > 0 ? (
                         data.map((accorItem) => (
-                            <div className='item'>
-                                <div className='title' onClick={""}>
+                            <div className='item' key={accorItem.id}>
+                                <div className='title' onClick={() => handleSingleQuestion(accorItem.id)}>
                                     <h4>{accorItem.question}</h4>
-                                    <span>+</span>
+                                    <span>{arrowIcon(accorItem.id)}</span>
                                 </div>
+                                {
+                                    selected === accorItem.id ? (
+                                        <div className='content'>{accorItem.answer}</div>
+                                    ): null
+                                }
                             </div>
                         ))
                     ):(
